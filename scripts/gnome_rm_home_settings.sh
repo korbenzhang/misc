7c9d649ffe61b1e0f5487be98bd9e2b4e58d0fdb
korbenzhang <korbenzhang@gmail.com>
2013-12-17 08:34:21 +0800

    add a script

---
+++ scripts/gnome_rm_home_settings.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# delete gnome default settings
+
+rm -rvf ~/.compiz-1/
+rm -rvf ~/.cache/compizconfig-1/
+rm -rvf ~/.cache/dconf/
+rm -rvf ~/.cache/unity/
+rm -vf ~/.cache/unity-lens-video
+rm -rvf ~/.config/compiz-1/
+rm -rvf ~/.config/dconf/
+rm -rvf ~/.config/gnome-control-center/
+rm -rvf ~/.config/gnome-session/
+rm -rvf ~/.gconf/
+rm -rvf ~/.gnome2/
+rm -rvf ~/.gnome2_private/
+rm -rvf ~/.gvfs/
+rm -rvf ~/.local/
+rm -rvf ~/.config/nautilus/
+
